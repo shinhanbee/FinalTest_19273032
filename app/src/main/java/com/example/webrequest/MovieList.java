1ae7941c8ba7df7dd9846365c3858355489e3484
shinhanbee <zeukuez@gmail.com>
2020-12-06 23:36:07 +0900

    캡스톤 디자인 기말 과제

---
+++ app/src/main/java/com/example/webrequest/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.webrequest;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/webrequest/MovieListResult.java
@@ -0,0 +1,12 @@
+package com.example.webrequest;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+
+}
